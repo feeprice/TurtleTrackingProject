6b5f7702f71a1badb4a149d8239102c03ebb3891
feeprice <fiprice23@gmail.com>
2025-09-22 12:46:13 -0400

    Building dictionaries

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -20,36 +20,29 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+#Create dictionaries
+date_dict = {}
+location_dict = {}
+
 #Pretend we read one line of data from the file
-#Iterate through all lines
 for lineString in line_list:
-    #Check if line has data
+    # Check if line is a data line
     if lineString[0] in ("#","u"):
         continue
 
     #Split the string into a list of data items
     lineData = lineString.split()
-   
+
     #Extract items in list into variables
     record_id = lineData[0]
     obs_date = lineData[2]
     obs_lc = lineData[4]
     obs_lat = lineData[6]
     obs_lon = lineData[7]
-   
+
+    #Add items to dictionaries
+    date_dict[record_id] = obs_date
+    location_dict[record_id] = (obs_lat, obs_lon)
+
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-
-#Split the string into a list of data items
-lineData = lineString.split()
-
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
-
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
